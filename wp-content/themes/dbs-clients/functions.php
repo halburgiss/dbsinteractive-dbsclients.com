828b194cb825281f72608aca6d0f4742a3fc5fd5
Hal Burgiss <hal@burgiss.net>
2021-02-24 14:26:55 -0500

    fix php error

--- wp-content/themes/dbs-clients/functions.php
+++ wp-content/themes/dbs-clients/functions.php
@@ -148,21 +148,4 @@ add_filter( 'upload_mimes', function ( $mimes ) {
 	return $mimes;
 }, 1 );
 
-if ( false ===  $dbs->has_search ) {
-	// disable search feature 2015-03-30
-	function fb_filter_query( $query, $error = true ) {
-	        if ( is_search() ) {
-	                $query->is_search = false;
-	                $query->query_vars[s] = false;
-	                $query->query[s] = false;
-	
-	                // to error
-	                if ( $error == true ) {
-	                        $query->is_404 = true;
-	                }
-	        }
-	}
-	add_action( "parse_query", __NAMESPACE__ .  "\\fb_filter_query" );
-	add_filter( "get_search_form", create_function( '$a', "return null;" ) );
-}
 
